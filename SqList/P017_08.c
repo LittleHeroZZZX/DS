fa9b692276fe55f3c8daccf6eef255e006f1176f
zhouxin <zhou.xin2000@outlook.com>
2023-08-18 14:05:14 +0800

    顺序表的序列交换

--- SqList/P017_08.c
+++ SqList/P017_08.c
@@ -1,5 +1,26 @@
 #include<stdio.h>
+#include "SqList.h"
 
-void swapSequence(int *array, int m, int n){
-    
+void swapSequence(SqList *L, int i, int j)
+{
+    // 交换顺序表中前i个元素和后j个元素的位置
+
+    // 逆置前i个元素、逆置后j个元素，再逆置整个顺序表
+    inverseSqList(L, 0, i-1);
+    inverseSqList(L, i, i+j-1);
+    inverseSqList(L, 0, i+j-1);
+
+}
+
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    SqList l;
+    initSqList(&l, arr, 6);
+    showSqList(l);
+    swapSequence(&l, 2, 4);
+    showSqList(l);
+
+
 }
